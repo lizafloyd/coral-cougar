27587a3ff9f12dc1def4948e510f95acc13f94a5
Liza Floyd <lizafloyd@Lizas-Air.localdomain>
2024-11-21 14:53:18 -0500

    adds mui and palette

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React, { Fragment } from 'react';
+import { Button, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+export function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#fbe3ef',
+      main: '#ee298D',
+      dark: '#ee0074',
+      contrastText: '#fff',
+      background: '#282c34'
+    },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Typography color={theme.palette.primary.light}></Typography>
+        <div className="App">
+          <header className="App-header">
+            <ButtonUsage />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div></ThemeProvider>
+
+    </>
   );
 }
 
